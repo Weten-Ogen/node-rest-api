b5333f8ebd67269098dfbbeb2be68e7593f42a53
Weten-Ogen <alphaoware@gmail.com>
2023-01-18 20:20:55 +0000

    added the static folder

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const http = require("http");
-const server = http.createServer((req,res) => {
-    console.log('starting server');
-    res.write("hello word");
-    
-});
+const express = require('express');
+const app = express();
+const path = require('path');
 
-server.listen(3000);
+
+// using static folders
+app.use(express.static('./public'))
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname,"./index.html"))
+})
+
+
+app.all('*', (req,res) =>{
+    res.status(404).send(`<h1>${req.url} not Found</h1>`)
+})
+app.listen(port=5000, () =>{
+    console.log(`Starting server on ${port}`)
+})
