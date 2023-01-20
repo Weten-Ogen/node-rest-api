275e6681b68b75cc2183bba46a1e92465dede259
Weten-Ogen <alphaoware@gmail.com>
2023-01-20 16:09:02 +0000

    worked with query in express js and also params

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
+const {products, people} = require('./data')
 
-// using static folders
-app.use(express.static('./public'))
+app.get('/' ,(req,res) =>{
+    res.status(200).send('<h1>Home Page</h1><a href="/api/products">products</a>')
+});
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname,"./index.html"))
+app.get('/api/products',(req,res) => {
+    const newProducts = products.map(product => {
+        const { id , name , price} = product;
+        return {id , name, price}
+    })
+    res.json(newProducts )
+});
+
+app.get('/api/products/:prodID',(req,res) => {
+    const { prodID } = req.params;
+    const item = products.find(product => product.id === Number(prodID))
+    if(!item){
+        res.status(404).send("Product Doesn't Exist")
+    }
+    res.json(item)  
+})
+
+app.get('/api/v1/query', (req,res) => {
+    console.log(req.query)
+    const { search , limit} = req.query
+    let sortedP = [...products]
+    if(search){
+        sortedP = sortedP.filter(product => {
+            return product.name.startsWith(search);
+        })
+    }
+    if(limit){
+        sortedP = sortedP.slice(0,Number(limit))
+    }
+    
+    res.status(404).json(sortedP)
 })
 
 
-app.all('*', (req,res) =>{
-    res.status(404).send(`<h1>${req.url} not Found</h1>`)
-})
-app.listen(port=5000, () =>{
-    console.log(`Starting server on ${port}`)
-})
+app.listen(4000,() => {
+    console.log("Server is Starting ...")
+});
---
+++ data.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id:1,
+        name: 'albany sofa',
+        price: 39.95,     
+    },
+    {
+        id:2,
+        name: 'entertainment center',
+        price: 29.98,     
+    },
+    {
+        id:3,
+        name: 'albany sectional',
+        price: 10.99,     
+    },
+    {
+        id:4,
+        name: 'leather sofa',
+        price: 9.99,     
+    },
+    {
+        id:1,
+        name: 'albany sofa',
+        price: 39.95,     
+    }
+]
+
+const people = [
+    {id : 1, name: 'Marcus'},
+    {id : 2, name: 'Enoch'},
+    {id : 3, name: 'Ronald'},
+    {id : 4, name: 'Hermione'},
+    {id : 5, name: 'Harry'},
+    {id : 6, name: 'Draco'},
+
+]
+module.exports = { people, products }
